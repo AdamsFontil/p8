cefafae40e474df5158c06cac06ae4de6fbabca5
Adams <adamsfuntil@gmail.com>
2025-11-26 14:57:05 -0500

    8.14 done with console.logs

--- graphQLServer/index.js
+++ graphQLServer/index.js
@@ -101,36 +101,40 @@ const resolvers = {
 allBooks: async (root, args) => {
   console.log('args given', args);
   const filter = {};
-
   if (args.author) {
     const author = await Author.findOne({ name: args.author });
     if (!author) return [];
     filter.author = author._id;
   }
-
   if (args.genre) {
     filter.genres = args.genre;
   }
-
   return Book.find(filter).populate("author");
 }
   },
     Mutation: {
-      addBook: (root, args) => {
-        console.log('what are args', args);
-        const authorExist = authors.some(author => author.name === args.author)
-        if (!authorExist) {
-          const newAuthor = { name: args.author, id: uuid() }
+      addBook: async (root, args) => {
+        const authorList = await Author.find({})
+        console.log('list of authors', authorList);
+        const authorExist = await Author.findOne({ name: args.author })
+        console.log('does author exist',authorExist);
+        if (authorExist === null) {
+          console.log('what are args received --- ', args);
+          const newAuthor = new Author({ name: args.author, born: null })
           console.log('new author detected adding this new author', newAuthor);
-          authors = authors.concat(newAuthor)
+          await newAuthor.save()
+          const newBook = new Book({ ...args, author: newAuthor.id })
+          console.log('what newBook created for new Author', newBook);
+        const savedBook = await newBook.save()
+        return savedBook.populate("author")
         }
+
         console.log('who is author', args.author);
         console.log('does author exist', authorExist);
-        const newBook = { ...args, id: uuid() }
+        const newBook = new Book({ ...args, author: authorExist.id })
         console.log('newBook', newBook);
-        console.log('authors list should change', authors);
-        books = books.concat(newBook)
-        return newBook
+        const savedBook = await newBook.save()
+        return savedBook.populate("author")
       },
       editAuthor: (root, args) => {
         const author = authors.find(a => a.name === args.name)
@@ -150,6 +154,47 @@ allBooks: async (root, args) => {
     }
   }
 }
+// Mutation: {
+//   addPerson: async (root, args) => {
+//       const person = new Person({ ...args })
+
+
+//       try {
+//         await person.save()
+//       } catch (error) {
+//         throw new GraphQLError('Saving person failed', {
+//           extensions: {
+//             code: 'BAD_USER_INPUT',
+//             invalidArgs: args.name,
+//             error
+//           }
+//         })
+//       }
+
+//       return person
+//   },
+//     editNumber: async (root, args) => {
+//       const person = await Person.findOne({ name: args.name })
+//       person.phone = args.phone
+
+
+//       try {
+//         await person.save()
+//       } catch (error) {
+//         throw new GraphQLError('Saving number failed', {
+//           extensions: {
+//             code: 'BAD_USER_INPUT',
+//             invalidArgs: args.name,
+//             error
+//           }
+//         })
+//       }
+
+//       return person
+//     }
+// }
+
+
 
 
 const server = new ApolloServer({
@@ -162,100 +207,3 @@ startStandaloneServer(server, {
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
-
-
-// let authors = [
-//   {
-//     name: 'Robert Martin',
-//     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-//     born: 1952,
-//   },
-//   {
-//     name: 'Martin Fowler',
-//     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-//     born: 1963
-//   },
-//   {
-//     name: 'Fyodor Dostoevsky',
-//     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-//     born: 1821
-//   },
-//   {
-//     name: 'Joshua Kerievsky', // birthyear not known
-//     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-//   },
-//   {
-//     name: 'Sandi Metz', // birthyear not known
-//     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-//   },
-// ]
-
-// /*
-//  * Suomi:
-//  * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
-//  * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
-//  *
-//  * English:
-//  * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
-//  * However, for simplicity, we will store the author's name in connection with the book
-//  *
-//  * Spanish:
-//  * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
-//  * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
-// */
-
-// let books = [
-//   {
-//     title: 'Clean Code',
-//     published: 2008,
-//     author: 'Robert Martin',
-//     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring']
-//   },
-//   {
-//     title: 'Agile software development',
-//     published: 2002,
-//     author: 'Robert Martin',
-//     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-//     genres: ['agile', 'patterns', 'design']
-//   },
-//   {
-//     title: 'Refactoring, edition 2',
-//     published: 2018,
-//     author: 'Martin Fowler',
-//     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring']
-//   },
-//   {
-//     title: 'Refactoring to patterns',
-//     published: 2008,
-//     author: 'Joshua Kerievsky',
-//     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring', 'patterns']
-//   },
-//   {
-//     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-//     published: 2012,
-//     author: 'Sandi Metz',
-//     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring', 'design']
-//   },
-//   {
-//     title: 'Crime and punishment',
-//     published: 1866,
-//     author: 'Fyodor Dostoevsky',
-//     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-//     genres: ['classic', 'crime']
-//   },
-//   {
-//     title: 'Demons',
-//     published: 1872,
-//     author: 'Fyodor Dostoevsky',
-//     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-//     genres: ['classic', 'revolution']
-//   },
-// ]
-
-// /*
-//   you can remove the placeholder query once your first one has been implemented
-// */
