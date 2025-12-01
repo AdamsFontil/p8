3924e2da713d83cb5831773d3d28984df452ffd8
Adams <adamsfuntil@gmail.com>
2025-12-01 14:50:11 -0500

    8.20

--- library-frontend-main/src/App.jsx
+++ library-frontend-main/src/App.jsx
@@ -4,6 +4,7 @@ import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import Notify from "./components/Notify";
 import LoginForm from "./components/LoginForm";
+import Recommend from "./components/Recommend";
 
 const App = () => {
   const [page, setPage] = useState("authors");
@@ -32,6 +33,7 @@ const App = () => {
         {token ? (
           <>
             <button onClick={() => setPage("add")}>add book</button>
+            <button onClick={() => setPage("recommend")}>recommend</button>
             <button onClick={logout}>logout</button>
           </>
         ) : (
@@ -48,6 +50,7 @@ const App = () => {
         setError={notify}
         setPage={setPage}
       />
+      <Recommend show={page === "recommend"} />
     </div>
   );
 };
---
+++ library-frontend-main/src/components/Recommend.jsx
@@ -0,0 +1,53 @@
+import { useQuery } from "@apollo/client/react";
+import { ALL_BOOKS } from "../queries";
+
+const Recommend = ({ show }) => {
+  console.log("what is show anyway", show);
+  const favoriteGenre = "patterns";
+
+  const result = useQuery(ALL_BOOKS);
+
+  // eslint-disable-next-line react/prop-types
+  if (!show) {
+    return null;
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+  const books = result.data.allBooks;
+
+  const filteredBooks = favoriteGenre
+    ? books.filter((book) => book.genres.includes(favoriteGenre))
+    : books;
+
+  console.log("filtred books", filteredBooks);
+
+  return (
+    <div>
+      <h2>Recommendations</h2>
+
+      <p>
+        books in your favorite genre <b>{favoriteGenre}</b>
+      </p>
+      <table>
+        <tbody>
+          <tr>
+            <th>title</th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {filteredBooks.map((book) => (
+            <tr key={book.id}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Recommend;
