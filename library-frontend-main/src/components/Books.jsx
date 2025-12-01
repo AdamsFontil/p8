18a855cb512518a16828b33225454848b324c005
Adams <adamsfuntil@gmail.com>
2025-12-01 14:23:25 -0500

    8.19

--- library-frontend-main/src/components/Books.jsx
+++ library-frontend-main/src/components/Books.jsx
@@ -1,8 +1,11 @@
 import { useQuery } from "@apollo/client/react";
 import { ALL_BOOKS } from "../queries";
+import { useState } from "react";
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS);
+  const [selectedGenre, setSelectedGenre] = useState(null);
+
   // eslint-disable-next-line react/prop-types
   if (!props.show) {
     return null;
@@ -14,11 +17,27 @@ const Books = (props) => {
   const books = result.data.allBooks;
   console.log("what is books", books);
   console.log("result from books comp", result);
+  const uniqueGenres = [...new Set(books.flatMap((book) => book.genres))];
+  console.log("unique genre are", uniqueGenres);
+
+  const filteredBooks =
+    selectedGenre && selectedGenre !== "all"
+      ? books.filter((book) => book.genres.includes(selectedGenre))
+      : books;
 
   return (
     <div>
       <h2>books</h2>
-
+      selected genre is currently --- {selectedGenre}
+      <h4>pick your genre</h4>
+      <div>
+        {uniqueGenres.map((target) => (
+          <button onClick={() => setSelectedGenre(target)} key={target}>
+            {target}
+          </button>
+        ))}
+        <button onClick={() => setSelectedGenre("all")}>all genres</button>
+      </div>
       <table>
         <tbody>
           <tr>
@@ -26,7 +45,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -40,3 +59,4 @@ const Books = (props) => {
 };
 
 export default Books;
+``;
