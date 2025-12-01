5535383cbafc8c5f242ad3c87f4a249ed5ccd37b
Adams <adamsfuntil@gmail.com>
2025-12-01 13:42:28 -0500

    8.18

--- library-frontend-main/src/App.jsx
+++ library-frontend-main/src/App.jsx
@@ -2,23 +2,50 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import Notify from "./components/Notify";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("testing");
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    // client.resetStore();
+  };
 
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
         <button onClick={() => setPage("add")}>add book</button>
+        {token ? (
+          <button onClick={logout}>logout</button>
+        ) : (
+          <button onClick={() => setPage("login")}>login</button>
+        )}
       </div>
 
       <Authors show={page === "authors"} />
-
       <Books show={page === "books"} />
-
       <NewBook show={page === "add"} />
+      <LoginForm
+        show={page === "login"}
+        setToken={setToken}
+        setError={notify}
+        setPage={setPage}
+      />
     </div>
   );
 };
---
+++ library-frontend-main/src/components/LoginForm.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { useMutation } from "@apollo/client/react";
+import { LOGIN } from "../queries";
+
+// eslint-disable-next-line react/prop-types
+const LoginForm = ({ setError, setToken, setPage }) => {
+  const [username, setUsername] = useState("ahellas");
+  const [password, setPassword] = useState("secret");
+
+  const [login, result] = useMutation(LOGIN, {
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message);
+    },
+  });
+
+  useEffect(() => {
+    if (result.data) {
+      const token = result.data.login.value;
+      setToken(token);
+      localStorage.setItem("user-token", token);
+    }
+  }, [result.data]);
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    try {
+      login({ variables: { username, password } });
+      setPage("authors");
+      setError("something is wrong");
+    } catch (error) {
+      console.log("trouble signing in", error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          username{" "}
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
